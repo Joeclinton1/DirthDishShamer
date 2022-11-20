85377619125f4a26de06c7bbc8a864396d109710
JoshuaJackNewbouldcd <JoshuaNewbould@outlook.com>
2022-11-20 02:46:14 +0000

    objectified face rec and obj detect

--- ML/objrec.py
+++ ML/objrec.py
@@ -1,6 +1,9 @@
 import torch
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
-def rec_objes(image):
-    return model.forward(image)
+class ObjectDetector():
+    def __init__(self) -> None:
+        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+    
+    def rec_objs(self, image):
+        return self.model.forward(image)
 
