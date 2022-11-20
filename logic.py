f0ea1141162482ef244681b582c3808cbb89993a
Joe Shepherd <sudo.jshepherd01@gmail.com>
2022-11-20 10:19:59 +0000

    Squish more bugs

--- logic.py
+++ logic.py
@@ -5,25 +5,25 @@ from skimage.metrics import structural_similarity
 
 def crop(img, bbox):
     # x1, x2, y1, y2 = bbox[0], bbox[2], bbox[1], bbox[3]
-    img = img[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+    img = img[bbox[2]:bbox[3], bbox[0]:bbox[1]]
     return img
 
 
 def get_similarity(dish, dishes2):
     # set weights
-    w1, w2, w3 = 0.5, 0.3, 0.2
+    w1, w2, w3 = 0.8, 0.2, 0
 
-    similarity = [0] * dishes2.shape[1]
+    similarity = [0] * len(dishes2)
     for i, dish2 in enumerate(dishes2):
         # calc similarity
-        pos1, pos2 = [d.pos for d in [dish, dish2]]
+        pos1, pos2 = [np.array(d.pos) for d in [dish, dish2]]
         area1, area2 = [d.area for d in [dish, dish2]]
-        ssim = structural_similarity(dish.img, dish2.img)
-        max_pos = (dish.img.shape[1] ** 2 + dish2.img.shape[2]) ** 0.5
+        # ssim = structural_similarity(dish.img, dish2.img)
+        max_pos = (dish.img.shape[1] ** 2 + dish2.img.shape[2]**2) ** 0.5
         max_area = dish.img.shape[0] * dish2.img.shape[1]
         max_ssim = 1
 
-        diff = (pos2 - pos1) / max_pos * w1 + (area2 - area1) / max_area * w2 + (1 - ssim / max_ssim) * w3
+        diff = np.linalg.norm(pos2 - pos1) / max_pos * w1 + (area2 - area1) / max_area * w2 # + (1 - ssim / max_ssim) * w3
         similarity[i] = 1 - diff / (w1 + w2 + w3)
     return similarity
 
@@ -37,7 +37,7 @@ def extract_table_contour(frame):
     thresh = 100
     ret, thresh_img = cv2.threshold(img_grey, thresh, 255, cv2.THRESH_BINARY)
     contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    return contours
+    return contours[0]
 
 
 class Obj():
@@ -57,7 +57,6 @@ class World():
         self.table = extract_table_contour(frame)
 
     def new_frame(self, objs: pd.DataFrame, frame_img):
-        print(objs)
         dishes_df = objs[objs["class"] != 0]
 
         # increment limbo for all dishes
@@ -67,23 +66,26 @@ class World():
             if dish.limbo >= 3:
                 del dish
 
-        for dish_df in dishes_df:
-            dish_bbox = [dish_df["x-min"], dish_df["x-max"], dish_df["y_min"], dish_df["y_max"]]
+        for _, dish_df in dishes_df.iterrows():
+            dish_bbox = list(map(int,[dish_df["xmin"], dish_df["xmax"], dish_df["ymin"], dish_df["ymax"]]))
             dish = Obj(dish_bbox, frame_img)
             if not obj_in_table_region(dish, self.table):
                 continue
 
-            for dishes2 in self.past_dishes:
-                sim_vals, dish_ids = get_similarity(dish, dishes2)
+            for dishes2 in self.past_dishes[::-1]:
+                if len(dishes2)==0: continue
+                sim_vals = get_similarity(dish, dishes2)
                 max_sim_idx = np.argmax(sim_vals)
 
                 if sim_vals[max_sim_idx] > self.sim_thresh:
-                    dish.limbo = 0
-                    break
-                else:
+                    del dishes2[max_sim_idx]
                     self.dishes.append(dish)
-                    return True
+                    break
+            else:
+                self.dishes.append(dish)
+                return True
 
+        if len(self.past_dishes) >= 2:
             self.past_dishes.pop(0)
-            self.past_dishes.append(self.dishes)
+        self.past_dishes.append(self.dishes)
         return False
--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import cv2
 from ML.objrec import ObjectDetector
 from ML.facerec import FaceRec
 
-load_dotenv()
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+# load_dotenv()
+# DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
-discord_bot = discord_shamer.DishDetectorBlocking(DISCORD_TOKEN)
+# discord_bot = discord_shamer.DishDetectorBlocking(DISCORD_TOKEN)
 facerec = FaceRec()
 objrec = ObjectDetector()
 # start main loop
@@ -37,13 +37,14 @@ while True:
 
     if is_dirty_dish_placed:
         # get name of shameful
-        people_df = obj_df[obj_df["class"] == 0]
-        bbox = [people_df["x-min"], people_df["x-max"], people_df["y_min"], people_df["y_max"]]
-        face_name = facerec.face_rec(frame[bbox[1]:bbox[3], bbox[0]:bbox[2]])
+        people_df = obj_df[obj_df["class"] == 0].iloc[0]
+        bbox = list(map(int, [people_df["xmin"], people_df["xmax"], people_df["ymin"], people_df["ymax"]]))
+        face_name = facerec.face_rec(frame[bbox[2]:bbox[3], bbox[0]:bbox[1]])
 
         # shame the shameful
+        print("SHAME! SHAME UPON", face_name)
         audio_shamer.shame()
-        discord_bot.shame()
+        # discord_bot.shame()
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
